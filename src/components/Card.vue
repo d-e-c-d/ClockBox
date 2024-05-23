4128a9ca270157feed3a0db975a483682e89941a
Dikoume Eboule Charles Darryl <charles.dikoume@2027.ucac-icam.com>
2024-05-23 22:59:13 +0100

    Initial

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+      class="flex flex-col items-center bg-white p-4 sm:p-6 justify-end rounded-3xl shadow-2xl w-full h-auto md:w-80 md:h-80 mb-6 md:mb-10 transform transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-3xl hover:z-10"
+  >
+    <img
+        v-if="img"
+        :src="img"
+        alt="Card Image"
+        class="w-24 h-24 sm:w-32 sm:h-32 lg:w-40 lg:h-40 object-cover mb-4 sm:mb-6 lg:mb-10"
+    />
+    <p
+        class="w-full sm:w-60 h-10 sm:h-12 px-2 sm:px-4 py-2 bg-blue-700 text-lg sm:text-xl text-white rounded-2xl hover:bg-blue-900 cursor-pointer text-center"
+    >
+      {{ title }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: String,
+  img: String,
+});
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Setting.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="bg-gray-100">
+    <Header />
+    <div class="min-h-screen flex flex-col">
+      <div class="flex flex-1">
+        <div class="flex flex-col justify-center items-center">
+          <Sidebar />
+        </div>
+
+        <div class="flex-1 p-12 grid grid-cols-1 md:grid-cols-4 gap-10 mx-28 mr-36 mt-2">
+          <Card
+              title="General"
+              img="src/components/icons/General.svg"
+          />
+          <Card
+              title="Leave Requests"
+              img="src/components/icons/Leave.svg"
+          />
+          <Card
+              title="View Subscription"
+              img="src/components/icons/Card.svg"
+          />
+          <Card
+              title="Sites"
+              img="src/components/icons/Sites.svg"
+          />
+          <Card
+              title="Positions"
+              img="src/components/icons/Positions.svg"
+          />
+          <Card
+              title="Schedules"
+              img="src/components/icons/Schedules.svg"
+          />
+          <Card
+              title="Contacts"
+              img="src/components/icons/Contacts.svg"
+          />
+          <Card
+              @click="openLogout"
+              title="LogOut"
+              img="src/components/icons/Logout.svg"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- LogOut -->
+  <transition name="fade">
+    <div v-if="isLogout" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div class="bg-white p-8 rounded-xl shadow-lg max-w-md w-full">
+        <h3 class="text-2xl bold font-bold text-blue-900"><span>LogOut</span></h3>
+        <p class="mt-2 text-sm text-gray-500">Do you want to disconnect ?</p>
+        <router-link to="/index" class="mt-4 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+          Exit
+        </router-link>
+        <button @click="closeLogout" class="mt-4 ml-5 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+          Cancel
+        </button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script setup lang="ts">
+import Card from '../components/Card.vue';
+import Header from '../components/Header.vue';
+import Sidebar from '../components/Sidebar.vue';
+import {ref} from "vue";
+
+const isLogout = ref(false);
+
+const closeLogout = () => {
+  isLogout.value = false;
+};
+const openLogout = () => {
+    isLogout.value = true;
+};
+
+</script>
