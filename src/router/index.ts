44a7d389c4b31a12e756078095c1a47bf5c7b5da
Dikoume Eboule Charles Darryl <charles.dikoume@2027.ucac-icam.com>
2024-05-29 18:20:14 +0100

    Initial

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/Login.vue';
 import Register from '../components/Register.vue';
+import Settings from "../components/Setting.vue";
+import Dashboard from "../components/Dashboard.vue";
 
 const routes = [
     { path: '/login', component: Login, name: 'ClockBox - Connexion' },
     { path: '/register', component: Register, name: 'ClockBox - Inscription' },
+    { path: '/settings', component: Settings, name: 'ClockBox - AdminSettings' },
+    { path: '/dashboard', component: Dashboard, name: 'ClockBox - AdminDashboard' },
+
+
 ];
 
 const router = createRouter({
